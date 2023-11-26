841a2ca8b9b7534c78190132c972ac18b862b8e2
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-11-26 23:22:32 +0100

    Fichier de configuration + initialisation des commandes

--- src/main/java/me/bluedyaishela/beltarium/Beltarium.java
+++ src/main/java/me/bluedyaishela/beltarium/Beltarium.java
@@ -1,13 +1,23 @@
 package me.bluedyaishela.beltarium;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class Beltarium extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        File configFile = new File(getDataFolder(), "config.yml");
+        if (!configFile.exists())
+        {
+            this.getLogger().info("Le fichier config.yml n'a pas été trouvé, création en cours...");
+            this.saveDefaultConfig();
+        } else {
+            this.getLogger().info("Le fichier de configuration a été trouvé, chargement en cours...");
+        }
+        FileConfiguration config = this.getConfig();
     }
 
     @Override
