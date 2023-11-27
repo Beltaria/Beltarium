edb49768dcaa312de0c392d316b3ca2a83ffb4a6
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-11-27 12:04:35 +0100

    Création de 4 items : - Casque en Beltarium - Plastron en Beltarium - Jambières en Beltarium - Bottes en Beltarium

--- src/main/java/me/bluedyaishela/beltarium/ItemManager.java
+++ src/main/java/me/bluedyaishela/beltarium/ItemManager.java
@@ -5,10 +5,12 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemManager {
+    public static List<String> getAllDeclaredFields;
     public static ItemStack BELTARIUM_NUGGET;
     public static ItemStack BELTARIUM_INGOT;
     public static ItemStack BELTARIUM_SWORD;
@@ -23,8 +25,23 @@ public class ItemManager {
 
     public static void init()
     {
+        initAllDeclaredFields();
         createBeltariumNugget();
         createBeltariumIngot();
+        createBeltariumHelmet();
+        createBeltariumChestplate();
+        createBeltariumLeggings();
+        createBeltariumBoots();
+    }
+
+    private static void initAllDeclaredFields()
+    {
+        Field[] fields = ItemManager.class.getDeclaredFields();
+        List<String> availableAttribute = new ArrayList<>();
+        for (Field field : fields) {
+            availableAttribute.add(field.getName());
+        }
+        getAllDeclaredFields = availableAttribute;
     }
 
     private static void createBeltariumNugget()
@@ -59,6 +76,77 @@ public class ItemManager {
         itemMeta.addEnchant(Enchantment.DURABILITY, 10, false);
         itemStack.setItemMeta(itemMeta);
         BELTARIUM_INGOT = itemStack;
+    }
+
+    private static void createBeltariumHelmet()
+    {
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_HELMET);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName("§4-= §cCasque en beltarium §4=-");
+
+        List<String> lore = new ArrayList<>();
+        lore.add("§cHaute température");
+
+        itemMeta.setLore(lore);
+        itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5, true);
+        itemMeta.addEnchant(Enchantment.DURABILITY, 4, true);
+        itemMeta.addEnchant(Enchantment.OXYGEN, 3, true);
+        itemMeta.addEnchant(Enchantment.WATER_WORKER, 1, true);
+        itemStack.setItemMeta(itemMeta);
+        BELTARIUM_HELMET = itemStack;
+    }
+
+    private static void createBeltariumChestplate()
+    {
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_CHESTPLATE);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName("§4-= §cPlastron en beltarium §4=-");
+
+        List<String> lore = new ArrayList<>();
+        lore.add("§cPeau de fer");
+
+        itemMeta.setLore(lore);
+        itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5, true);
+        itemMeta.addEnchant(Enchantment.DURABILITY, 4, true);
+        itemStack.setItemMeta(itemMeta);
+        BELTARIUM_CHESTPLATE = itemStack;
+    }
+
+    private static void createBeltariumLeggings()
+    {
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_LEGGINGS);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName("§4-= §cJambières en beltarium §4=-");
+
+        List<String> lore = new ArrayList<>();
+        lore.add("§cVitesse améliorée");
+
+        itemMeta.setLore(lore);
+        itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5, true);
+        itemMeta.addEnchant(Enchantment.DURABILITY, 4, true);
+        itemStack.setItemMeta(itemMeta);
+        BELTARIUM_LEGGINGS = itemStack;
+    }
+
+    private static void createBeltariumBoots()
+    {
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_BOOTS);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName("§4-= §cBottes en beltarium §4=-");
+
+        List<String> lore = new ArrayList<>();
+        lore.add("§cChute amortie");
+
+        itemMeta.setLore(lore);
+        itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5, true);
+        itemMeta.addEnchant(Enchantment.DURABILITY, 4, true);
+        itemMeta.addEnchant(Enchantment.DEPTH_STRIDER, 3, true);
+        itemStack.setItemMeta(itemMeta);
+        BELTARIUM_BOOTS = itemStack;
     }
 
 }
